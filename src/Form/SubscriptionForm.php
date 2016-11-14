eb87158aaa9112db2e5d388ef216556cb199bcf6
Silver <info@filinkov.ru>
2016-11-14 21:45:55 +0500

    Исправление ошибок и изменение структуры базы данных.

--- src/Form/SubscriptionForm.php
+++ src/Form/SubscriptionForm.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * @file
- * Contains \Drupal\site_subscription\Form\SubscriptionForm.
- *
- * В комментарии выше указываем, что содержится в данном файле.
- */
+* @file
+* Contains \Drupal\site_subscription\Form\SubscriptionForm.
+*
+* В комментарии выше указываем, что содержится в данном файле.
+*/
 
 // Объявляем пространство имён формы. Drupal\site_subscription\Form
 namespace Drupal\site_subscription\Form;
@@ -15,87 +15,83 @@ use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Объявляем нашу форму, наследуясь от FormBase.
- * Название класса строго должно соответствовать названию файла.
- */
-class SubscriptionForm extends FormBase {
-
-  /**
-   * То что ниже - это аннотация. Аннотации пишутся в комментариях и в них
-   * объявляются различные данные. В данном случае указано, что документацию
-   * к данному методу надо взять из комментария к самому классу.
-   *
-   * А в самом методе мы возвращаем название нашей формы в виде строки.
-   * Эта строка используется для альтера формы (об этом ниже в тексте).
-   *
-   * {@inheritdoc}.
-   */
-  public function getFormId() {
-    return 'subscription_form';
-  }
-
-  /**
-   * Создание нашей формы.
-   *
-   * {@inheritdoc}.
-   */
-  public function buildForm(array $form, FormStateInterface $form_state) {
-    // Объявляем телефон.
-    $form['email'] = array(
-      '#type' => 'email',
-      '#title' => $this->t('Enter your E-mail'),
-      '#description' => $this->t('A valid e-mail address.'),
-      '#attributes' => array('placeholder' => $this->t('user@mydomain.com'), 'class' => ['form-control']),
-    );
-
-    // Предоставляет обёртку для одного или более Action элементов.
-    $form['actions']['#type'] = 'actions';
-    // Добавляем нашу кнопку для отправки.
-    $form['actions']['submit'] = array(
-      '#type' => 'submit',
-      '#value' => $this->t('Subscribe'),
-      '#button_type' => 'primary',
-    );
-    return $form;
-  }
-
-  /**
-   * Валидация отправленых данных в форме.
-   *
-   * {@inheritdoc}
-   */
-  public function validateForm(array &$form, FormStateInterface $form_state) {
-    return \Drupal::service('email.validator')->isValid($email);
-  }
-
-  /**
-   * Отправка формы.
-   *
-   * {@inheritdoc}
-   */
-  public function submitForm(array &$form, FormStateInterface $form_state) {
-  $email = $form_state->getValue('email');
-
-      if($email != mail){
-  $uuid = \Drupal::service('uuid');
-  db_insert('site_subscription')
-  ->fields(array(
-    'uuid' => $uuid->generate(),
-    'uid' => 0,
-//    'mail' => $form_state->getValue('mail'),
-    'mail' => serialize(array('email' => $form_state->getValue('email')) ),
-//    'status' => 0,
-    'created' => REQUEST_TIME,
-//    'updated' => REQUEST_TIME,
-  ))
-  ->execute();
-  drupal_set_message($this->t('Your e-mail has been sent'));
-  }else{
-  drupal_set_message($this->t('Your e-mail has been sent to'));
-  }
- }
+* Объявляем нашу форму, наследуясь от FormBase.
+* Название класса строго должно соответствовать названию файла.
+*/
+class SubscriptionForm extends FormBase {    
+    /**
+    * {@inheritdoc}.
+    */
+    public function getFormId() {
+        return 'site_subscription_form';
+    }
+    
+    /**
+    * Создание нашей формы.
+    *
+    * {@inheritdoc}.
+    */
+    public function buildForm(array $form, FormStateInterface $form_state) {
+        // Объявляем телефон.
+        $form['mail'] = array(
+        '#type' => 'email',
+        '#title' => $this->t('Enter your E-mail'),
+        '#description' => $this->t('A valid e-mail address.'),
+        '#attributes' => array('placeholder' => $this->t('user@mydomain.com'), 'class' => ['form-control']),
+        );
+        
+        // Предоставляет обёртку для одного или более Action элементов.
+        $form['actions']['#type'] = 'actions';
+        
+        // Добавляем нашу кнопку для отправки.
+        $form['actions']['submit'] = array(
+        '#type' => 'submit',
+        '#value' => $this->t('Subscribe'),
+        '#button_type' => 'primary',
+        );
+        
+        return $form;
+    }
+    
+    /**
+    * Валидация отправленых данных в форме.
+    *
+    * {@inheritdoc}
+    */
+    public function validateForm(array &$form, FormStateInterface $form_state) {
+        return \Drupal::service('email.validator')->isValid($mail);
+    }
+    
+    /**
+    * Отправка формы.
+    *
+    * {@inheritdoc}
+    */
+    public function submitForm(array &$form, FormStateInterface $form_state) {
+        $account = \Drupal::currentUser();
+				$connection = \Drupal::database();
+        
+        if ($account->id()) {
+            $connection->insert('site_subscription')
+            ->fields([
+            'uid' => $account->id(),
+            'type' => 'all',
+            'type_id' => 0,
+            'mail' => $account->getEmail(),
+            'created' => REQUEST_TIME,
+            ])
+            ->execute();
+        }
+        else {
+            $connection->insert('site_subscription')
+            ->fields([
+            'uid' => 0,
+            'type' => 'all',
+            'type_id' => 0,
+            'mail' => $form_state->getValue('mail'),
+            'created' => REQUEST_TIME,
+            ])
+            ->execute();
+        }
+    }
 }
-
-//    drupal_set_message($this->t('Thank you @email.', array(
-//      '@email' => $form_state->getValue('email'),
-//    )))
