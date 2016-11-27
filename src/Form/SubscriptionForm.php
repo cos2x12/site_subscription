d26d6f4f8269482b4a1079cabea86eae4c6d0d6f
Silver <info@filinkov.ru>
2016-11-27 21:07:30 +0500

    Внесены исправления

--- src/Form/SubscriptionForm.php
+++ src/Form/SubscriptionForm.php
@@ -25,30 +25,27 @@ class SubscriptionForm extends FormBase {
     public function getFormId() {
         return 'site_subscription_form';
     }
-
+    
     /**
     * Создание нашей формы.
     *
     * {@inheritdoc}.
     */
     public function buildForm(array $form, FormStateInterface $form_state) {
-        // Объявляем телефон.
+        $account = \Drupal::currentUser();
+
+        // Объявляем e-mail.
         $form['mail'] = array(
-            '#type' => 'email',
-            '#title' => $this->t('Enter your E-mail'),
-            '#description' => $this->t('A valid e-mail address.'),
-            '#attributes' => array('placeholder' => $this->t('user@mydomain.com'), 'class' => ['form-control']),
-        );
-        $form['type_subscription'] = array(
-            '#type' => 'checkboxes',
-            '#options' => array('NEWS' => $this->t('News'), 'COURSES' => $this->t('Courses')),
-            '#title' => $this->t('Choose subscription type'),
-            '#default_value' => 0,
+        '#type' => 'email',
+        '#title' => $this->t('Enter your E-mail'),
+        '#description' => $this->t('A valid e-mail address.'),
+        '#attributes' => array('placeholder' => $this->t('user@mydomain.com'), 'class' => ['form-control']),
+        '#access' => $account->id() ? FALSE : TRUE,
         );
 
         // Предоставляет обёртку для одного или более Action элементов.
         $form['actions']['#type'] = 'actions';
-        
+
         // Добавляем нашу кнопку для отправки.
         $form['actions']['submit'] = array(
         '#type' => 'submit',
@@ -65,7 +62,7 @@ class SubscriptionForm extends FormBase {
     * {@inheritdoc}
     */
     public function validateForm(array &$form, FormStateInterface $form_state) {
-        return \Drupal::service('email.validator')->isValid($email);
+        return \Drupal::service('email.validator')->isValid($form_state->getValue('mail'));
     }
     
     /**
@@ -76,43 +73,43 @@ class SubscriptionForm extends FormBase {
     public function submitForm(array &$form, FormStateInterface $form_state) {
         $account = \Drupal::currentUser();
         $connection = \Drupal::database();
-        $options = $form_state->getValue('options');
-        dpm($options);
+
         $result = $connection->select('site_subscription', 'n')
         ->fields('n', array('mail'))
         ->condition('n.mail', $form_state->getValue('mail'));
         $mail = $result->execute()->fetchField();
         // Execute the statement
+
         $result1 = $connection->select('site_subscription', 'n')
         ->fields('n', array('uid'))
         ->condition('n.uid', $account->id());
         $uid = $result1->execute()->fetchField();
 
         if ($mail == $form_state->getValue('mail') && $account->id() == $uid) {
-            drupal_set_message($this->t('Your e-mail has been sent to'));
+            drupal_set_message($this->t('You are subscribed'));
         }
         elseif ($account->id()) {
             $connection->insert('site_subscription')
             ->fields([
-            'uid' => $account->id(),
-            'type' => 'all',
-            'type_id' => '0',
-            'mail' => $account->getEmail(),
-            'created' => REQUEST_TIME,
+                'uid' => $account->id(),
+                'type' => 'all',
+                'type_id' => '0',
+                'mail' => $account->getEmail(),
+                'created' => REQUEST_TIME,
             ])
             ->execute();
         }
         else {
             $connection->insert('site_subscription')
             ->fields([
-            'uid' => 0,
-            'type' => 'all',
-            'type_id' => '0',
-            'mail' => $form_state->getValue('mail'),
-            'created' => REQUEST_TIME,
+                'uid' => 0,
+                'type' => 'all',
+                'type_id' => '0',
+                'mail' => $form_state->getValue('mail'),
+                'created' => REQUEST_TIME,
             ])
             ->execute();
         }
-
+        
     }
 }
